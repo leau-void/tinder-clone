3c7e29d88cecdbda85e028c56ad227936370b06e
leau-void <lpierra@gmail.com>
2021-11-02 17:37:10 -0300

    add settings icon and setup route

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,6 +3,18 @@ import React from "react";
 import { Switch, Route } from "react-router";
 import { Link } from "react-router-dom";
 import NavBar from "./NavBar";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCog } from "@fortawesome/free-solid-svg-icons";
+
+const Icon = styled(FontAwesomeIcon)`
+  font-size: 1.7rem;
+  color: grey;
+
+  &:hover {
+    color: #46cdcf;
+  }
+`;
 
 const Main = () => {
   return (
@@ -20,7 +32,9 @@ const Main = () => {
         </Route>
       </Switch>
       <button onClick={() => signOut(getAuth())}>Logout</button>
-      <Link to="/settings">Settings</Link>
+      <Link to="/settings">
+        <Icon icon={faCog} />
+      </Link>
       <NavBar />
     </div>
   );
